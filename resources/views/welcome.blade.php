bd51bc239b72c465e3c37e159c9514d440e89e1b
luicahleo <luisrolandocahuanaleon@gmail.com>
2020-12-06 00:07:56 +0100

    probando git con laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Prueba
+                    Prueba ya cargo
                 </div>
 
                 <div class="links">
